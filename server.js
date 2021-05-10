feea6ccf2cdfc63b773f7fd0fe317de665e4981c
Christian Wittorp <cwwittor@ncsu.edu>
2021-05-10 00:13:54 -0400

    Changed server file

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const expHandlebars = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const routes = require("./controllers/burgers_controller.js");
+const expHandlebars = require("express-handlebars");
 
-app.use(express.static("public"));
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+app.use(express.static("public"));
 
 app.engine("handlebars", expHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Currently listening on Port: ${PORT}`));
